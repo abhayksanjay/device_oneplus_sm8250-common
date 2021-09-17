528782fe213a3ad8c496c1a77aa1b3eb4d8942c2
ugly-kid-af <dwarmachine24@gmail.com>
2021-09-17 10:59:11 +0530

    sm8250-common: Remove Packages: Spare some apps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -8,4 +8,6 @@ LOCAL_OVERRIDES_PACKAGES := NfcNci Photos RecorderPrebuilt SafetyHubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM Tycho USCCDM VZWAPNLib
 include $(BUILD_PREBUILT)
